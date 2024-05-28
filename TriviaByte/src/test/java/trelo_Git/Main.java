4daa381fe1e5932ac5c12975706c6392806dd0dc
Alons0-M <alonsoamadorarredondo@gmail.com>
2024-05-28 11:15:18 +0200

    Change from zip to file

---
+++ TriviaByte/src/test/java/trelo_Git/Main.java
@@ -0,0 +1,8 @@
+package trelo_Git;
+public class Main {
+    public static void main(String[] args) {
+        QuizManager quizManager = new QuizManager();
+        quizManager.setupGame();
+        quizManager.startGame();
+    }
+}

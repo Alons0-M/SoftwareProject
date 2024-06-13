4b47b8adab9cdc736eccec43a6d180993250d7a7
Alemediii <alemedii04@gmail.com>
2024-06-13 19:37:16 +0200

    Modified class Experiment10 [some things to do]

--- TriviaByte/src/main/java/trelo_Git/Main.java
+++ TriviaByte/src/main/java/trelo_Git/Main.java
@@ -3,9 +3,9 @@ package trelo_Git;
 public class Main {
 
 	public static void main(String[] args) {
-		FeedBack feedback = new FeedBack();
+	//	FeedBack feedback = new FeedBack();
 	//	HomePage homepage = new HomePage();
-	//	Experiment10 experiment10 = new Experiment10();
+		Experiment10 experiment10 = new Experiment10();
 	}
 
 }
